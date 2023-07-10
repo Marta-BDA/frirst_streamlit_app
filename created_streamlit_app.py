fe9e3a49bc067f9b20b368bed8138cddf11f248a
Marta N.- Data Analyst <116729581+Marta-BDA@users.noreply.github.com>
2023-07-10 09:28:29 +0200

    Update created_streamlit_app.py
    mayusculas todo

--- created_streamlit_app.py
+++ created_streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title ('my parents new healthly diner')
 streamlit.header ('breakfast menu')
-streamlit.text ('Omega3 and blueberry oatmeal')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
 streamlit.text ('Hard-Boiled Free-Range Egg')
