f0779fc549d15dbbafbf0c88adf809465ba2d9c6
Marta N.- Data Analyst <116729581+Marta-BDA@users.noreply.github.com>
2023-07-10 09:53:48 +0200

    Update created_streamlit_app.py
    error corregido

--- created_streamlit_app.py
+++ created_streamlit_app.py
@@ -16,5 +16,5 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-stremalit.dataframe ('my_fruit_list')
+streamlit.dataframe ('my_fruit_list')
 
