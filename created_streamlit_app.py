cb9b4bb3b71fca6b39b7e64e20572340047ef811
Marta N.- Data Analyst <116729581+Marta-BDA@users.noreply.github.com>
2023-07-10 09:26:36 +0200

    Update created_streamlit_app.py
    fixed typo

--- created_streamlit_app.py
+++ created_streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title ('my parents new healthly diner')
+streamlit.header ('breakfast menu')
+streamlit.text ('Omega3 and blueberry oatmeal')
+streamlit.text ('Kale, spinach and rocket smoothie')
+streamlit.text ('hard boiled and free range egg')
