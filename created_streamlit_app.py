837a8a943bd320752ae379bbc31195aa34e066fb
Marta N.- Data Analyst <116729581+Marta-BDA@users.noreply.github.com>
2023-07-13 11:00:22 +0200

    Update created_streamlit_app.py
    ordenado -1

--- created_streamlit_app.py
+++ created_streamlit_app.py
@@ -23,28 +23,22 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
-# Display the table on the page.
-streamlit.dataframe (fruits_to_show)
 
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
-#streamlit.text(fruityvice_response)
-
-streamlit.header("Fruityvice Fruit Advice!")
-#streamlit.text(fruityvice_response.json())
-
-#pasa el json a tabla 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# enseña el df
-streamlit.dataframe(fruityvice_normalized)
-
+###
+#new section to display
 #variable fruta elegida
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-streamlit.stop()
+streamlit.header ('Fruityvice Fruit Advice')
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+  streamlit.error ('Please select a fruit to get information.')
+ else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  fruityvice_normalized=pandas.json_normalize (fruityvice_response.json())
+  streamlit.dataframe (fruityvice_normalized)
+ except:
+  streamlit.error()
+  
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
